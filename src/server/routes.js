8d456b480280c2b1bcf051c959bcce7652a9a725
Ardian Pramana Putra <ardianpramana1802@gmail.com>
2024-12-10 14:44:50 +0700

    Commit kelima

--- src/server/routes.js
+++ src/server/routes.js
@@ -7,8 +7,8 @@ const routes = [
     handler: async (request, h) => {
       const { image } = request.payload;
 
-      // Validasi apakah image dan image._data ada
-      if (!image || !image._data || image._data.length > 1000000) {
+      // 1. Periksa ukuran file terlebih dahulu (hanya untuk payload lebih dari 1MB)
+      if (image && image._data && image._data.length > 1000000) {
         const response = h.response({
           status: 'fail',
           message: 'Payload content length greater than maximum allowed: 1000000',
@@ -17,6 +17,7 @@ const routes = [
         return response;
       }
 
+      // 2. Jika file kurang dari 1MB, lanjutkan dengan prediksi
       return postPredictHandler(request, h);
     },
     options: {
